75e1033fe5c3bbcb7ef0d708e785e4d5732509f8
shamon2654 <shamons226@gmail.com>
2024-01-09 16:14:32 +0530

    feat:using two context

--- src/Component/Navigation.jsx
+++ src/Component/Navigation.jsx
@@ -1,25 +1,68 @@
-import React, { Component } from 'react'
-import { ThemeContext } from '../Context/themContext'
-import ThemeToggle from './ThemeToggle';
+import React, { Component } from "react"
+import { ThemeContext } from "../Context/themContext"
+import ThemeToggle from "./ThemeToggle"
+import { AuthContext } from "../Context/AuthContext"
 
 export class Navigation extends Component {
-  static contextType = ThemeContext;//access the themecontext properties using static
+  static contextType = ThemeContext //access the themecontext properties using static
   render() {
     // console.log(this.context)//props of the context syntax
-    const { isLightTheme, dark, light } = this.context
-    const theme = isLightTheme ? light : dark;
+    // const { isLightTheme, dark, light } = this.context
+    // const theme = isLightTheme ? light : dark;
+    // return (
+    //   <nav style={{background:theme.ui , color:theme.syntax,display:"flex" ,justifyContent:"space-between"}}>
+    //     <ul>
+    //       <li>Home</li>
+    //       <li>About</li>
+    //       <li>Contact</li>
+    //     </ul>
+    //     <ul>
+    //     <li ><ThemeToggle/></li>
+    //     </ul>
+
+    //   </nav>
+    // )
     return (
-      <nav style={{background:theme.ui , color:theme.syntax,display:"flex" ,justifyContent:"space-between"}}>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-        <ul>
-        <li ><ThemeToggle/></li>
-        </ul>
-        
-      </nav>
+      <AuthContext.Consumer>
+        {(
+          authContext //using multiple context
+        ) => (
+          <ThemeContext.Consumer>
+            {(context) => {
+              //another way to get the context data or consume the data
+              const { isLightTheme, dark, light } = context
+              console.log(context)
+              const theme = isLightTheme ? light : dark
+              console.log(theme)
+              const { isLogin,toggleAuth } = authContext;
+              return (
+                <nav
+                  style={{
+                    background: theme.ui,
+                    color: theme.syntax,
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <h1>Context Api</h1>
+                  <div onClick={toggleAuth}>{isLogin? "LogeddIn":"LoggedOut"}</div>
+                  <button onClick={toggleAuth}>{isLogin ? "Login":"Logout"}</button>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <ThemeToggle />
+                    </li>
+                  </ul>
+                </nav>
+              )
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     )
   }
 }
